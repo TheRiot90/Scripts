42b986f748617db8443a7f04fb99a8cd7c72523e
Alfred Theriot <52082557+TheRiot90@users.noreply.github.com>
2022-12-16 18:20:25 -0600

    Create leap year.py
    Based on user input will tell you if the year is a leap year.

---
+++ Python/leap year.py
@@ -0,0 +1,11 @@
+is_leap_year = False
+
+input_year = int(input())
+
+if (input_year % 4) == 0:
+    if (input_year % 100 == 0) and (input_year % 400 != 0):
+        print("{} - not a leap year".format(input_year))
+    else:
+        print("{} - leap year".format(input_year))
+else:
+    print("{} - not a leap year".format(input_year))
